bef14f54c585d43b8c7944e319748217018ee7c0
Bhavya700 <116048013+Bhavya700@users.noreply.github.com>
2025-04-08 22:54:44 -0700

    Split test cases from test.py into main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+from test import seating_students
+
+# Test cases
+print(seating_students([12, 2, 6, 7, 11]))  # Expected Output: 6
+print(seating_students([6, 4]))            # Expected Output: 4
+print(seating_students([8, 1, 8]))         # Expected Output: 6
--- test.py
+++ test.py
@@ -30,7 +30,3 @@ def seating_students(arr):
 
   return count
 
-# Test cases
-print(seating_students([12, 2, 6, 7, 11]))  # Output: 6
-print(seating_students([6, 4]))  # Output: 4
-print(seating_students([8, 1, 8]))  # Output: 6
